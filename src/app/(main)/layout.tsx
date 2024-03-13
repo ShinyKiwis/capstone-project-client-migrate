e58c113f9d7784a7864322114d62285dfeecd708
Nguyen Hua <nguyenhua.hcm@gmail.com>
2024-03-13 15:57:29 +0700

    Implement useAuth

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,12 +1,23 @@
+"use client"
+import { useEffect } from "react";
 import App from "../_components/App";
 import DeadlinesProvider from "../providers/DeadlinesProvider";
 import RolesProvider from "../providers/RolesProvider";
+import { useAuth } from "../providers/AuthProvider";
+import useNavigate from "../hooks/useNavigate";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const { user } = useAuth();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!user) {
+      navigate("/login");
+    }
+  }, []);
   return (
     <DeadlinesProvider>
       <RolesProvider>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@mantine/core/styles.css";
 import "mantine-datatable/styles.css";
-import '@mantine/notifications/styles.css';
-import '@mantine/dates/styles.css'
-import '@mantine/dropzone/styles.css';
+import "@mantine/notifications/styles.css";
+import "@mantine/dates/styles.css";
+import "@mantine/dropzone/styles.css";
 import "./globals.css";
 
 import { ColorSchemeScript, MantineProvider } from "@mantine/core";
@@ -12,6 +12,7 @@ import { ModalsProvider } from "@mantine/modals";
 
 import PageTitleProvider from "./providers/PageTitleProvider";
 import { Notifications } from "@mantine/notifications";
+import AuthProvider from "./providers/AuthProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -32,10 +33,12 @@ export default function RootLayout({
       </head>
       <body className={inter.className}>
         <MantineProvider defaultColorScheme="light">
-          <ModalsProvider>
-            <Notifications />
-            <PageTitleProvider>{children}</PageTitleProvider>
-          </ModalsProvider>
+          <AuthProvider>
+            <ModalsProvider>
+              <Notifications />
+              <PageTitleProvider>{children}</PageTitleProvider>
+            </ModalsProvider>
+          </AuthProvider>
         </MantineProvider>
       </body>
     </html>
